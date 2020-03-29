0d113fcfe29ef8466342eb8ade11153d5d3e5807
Murat Benli <muratbenli001@gmail.com>
2020-03-29 15:24:57 +0300

    book-details n some actions add

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
     return [
-        { title: 'Javascript: Tho Good Parts'},
-        { title: 'Harry Potter'},
-        { title: 'The Dark Tower'},
-        { title: 'Clean code'},
-        { title: 'Scrum Guide'}
+        { title: 'Javascript: Tho Good Parts', pages: 101},
+        { title: 'Harry Potter', pages: 39},
+        { title: 'The Dark Tower', pages: 85},
+        { title: 'Clean code', pages: 212},
+        { title: 'Scrum Guide', pages: 21}
     ]
 }
