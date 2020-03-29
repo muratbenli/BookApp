5eaf6e4c7563648ff6ef5b91a74eb68033162105
Murat Benli <muratbenli001@gmail.com>
2020-03-29 12:21:54 +0300

    booklist comp. add

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Javascript: Tho Good Parts'},
+        { title: 'Harry Potter'},
+        { title: 'The Dark Tower'},
+        { title: 'Clean code'},
+        { title: 'Scrum Guide'}
+    ]
+}
